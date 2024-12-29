7aaac4b64d1429a0460b934bfd76fde97edbe551
Edwin Leon <ridergaleon@yahoo.com>
2024-12-29 22:59:56 +1000

    feat: add technologies section

---
+++ src/consts.js
@@ -0,0 +1,99 @@
+import IconHTML from "./components/icons/technologies/IconHTML.astro";
+import IconCSS from "./components/icons/technologies/IconCSS.astro";
+import IconTailwind from "./components/icons/technologies/IconTailwind.astro";
+import IconJS from "./components/icons/technologies/IconJS.astro";
+import IconReact from "./components/icons/technologies/IconReact.astro";
+import IconAstro from './components/icons/technologies/IconAstro.astro';
+import IconGit from './components/icons/technologies/IconGit.astro';
+import IconGithub from './components/icons/technologies/IconGithub.astro';
+import IconTS from './components/icons/technologies/IconTS.astro';
+import IconNode from "./components/icons/technologies/IconNode.astro";
+import IconPython from "./components/icons/technologies/IconPython.astro";
+import IconSapUI5 from "./components/icons/technologies/IconSapUI5.astro";
+import IconSAP from "./components/icons/technologies/IconSAP.astro";
+import IconSQL from "./components/icons/technologies/IconSQL.astro";
+
+
+export const TODAS_TECNOLOGIAS = [
+    {
+        nombre: "TypeScript",
+        icono: "TypeScript",
+        color: "#3078c6",
+    },
+    {
+        nombre: "JavaScript",
+        icono: "JavaScript",
+        color: "#F0DB4F",
+    },
+    {
+        nombre: "Python",
+        icono: "Python",
+        color: "#387EB8",
+    },
+    {
+        nombre: "SQL",
+        icono: "SQL",
+        color: "#1B9BDB",
+    },
+    {
+        nombre: "HTML",
+        icono: "HTML",
+        color: "#EF652A",
+    },
+    {
+        nombre: "CSS",
+        icono: "CSS",
+        color: "#30A9DC",
+    },
+    {
+        nombre: "NodeJs",
+        icono: "NodeJs",
+        color: "#41873F",
+    },
+    {
+        nombre: "React",
+        icono: "React",
+        color: "#61DAFB",
+    },
+    {
+        nombre: "Astro",
+        icono: "Astro",
+        color: "#FF5D01",
+    },
+    {
+        nombre: "Tailwind CSS",
+        icono: "Tailwind",
+        color: "#38B2AC",
+    },
+    {
+        nombre: "Git",
+        icono: "Git",
+        color: "#de4b36",
+    },
+    {
+        nombre: "GitHub",
+        icono: "GitHub",
+        color: "#fff",
+    },
+    {
+        nombre: "SAP BTP",
+        icono: "SAPBTP",
+        color: "#00AEEF",
+    },
+]
+
+export const TECNOLOGIAS_ICONOS = {
+    HTML: IconHTML,
+    CSS: IconCSS,
+    Tailwind: IconTailwind,
+    JavaScript: IconJS,
+    TypeScript: IconTS,
+    React: IconReact,
+    Astro: IconAstro,
+    Git: IconGit,
+    GitHub: IconGithub,
+    NodeJs: IconNode,
+    Python: IconPython,
+    SAPBTP: IconSAP,
+    SQL: IconSQL
+};
