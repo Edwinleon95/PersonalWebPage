b775b73789af7bb1a4f131169fc09fa3a9b53bb5
Edwin Leon <ridergaleon@yahoo.com>
2025-04-26 10:07:54 -0500

    update: improve descriptions and fix image error and styles

--- src/data/projects.ts
+++ src/data/projects.ts
@@ -2,7 +2,7 @@ export const PROJECTS = [
     {
         title: "Think & Guess",
         description: "Think & Guess is an exciting multiplayer party game where players compete to guess words based on creative prompts from the host. Designed for fun and fast-paced interaction, this full-stack project delivers a smooth and engaging experience, making it perfect for game nights and social events.",
-        image: "./Think&Guess.png",
+        image: "/Think&Guess.png",
         link: "https://doodleguessfrontend-production.up.railway.app",
         github: "https://github.com/Edwinleon95/Think_Guess_Host",
         features: [
@@ -15,7 +15,7 @@ export const PROJECTS = [
     {
         title: "Pokédex AI",
         description: "A modern Pokédex built with React, TypeScript, and Vite, featuring AI-generated Pokémon descriptions and real-time data from the Pokémon API.",
-        image: "./pokedex.png",  // Update with your actual image
+        image: "/pokedex.png",  // Update with your actual image
         link: "https://pokedexiaprojectv4.netlify.app/",
         github: "https://github.com/Edwinleon95/FrontEndProject",
         features: [
